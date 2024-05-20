6babe99e3f680a5975dd1f86b39563f97c5d8efe
eljily <eljilysidibrahim@gmail.com>
2024-05-20 22:23:17 +0000

    added data.json to assets

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,6 +1,8 @@
 import {
   Component,
   computed,
+  inject,
+  OnInit,
   Signal,
   signal,
   WritableSignal,
@@ -16,6 +18,7 @@ import { IEntreprise } from '../../models/entreprise.model';
 import { EntrepriseCardComponent } from '../entreprise-card/entreprise-card.component';
 import { SocialMediaPlatform } from '../../models/social-media-plateform.enum';
 import { City } from '../../models/cities.enum';
+import { EntrepriseService } from '../../services/entreprise.service';
 
 @Component({
   selector: 'app-hero',
@@ -27,13 +30,22 @@ import { City } from '../../models/cities.enum';
     provideIcons({ bootstrapSearch, bootstrapBuilding, bootstrapStack }),
   ],
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
   technology: WritableSignal<string> = signal('');
-  #allEntreprises: IEntreprise[] = this.getFakeEntrepriseData();
+  #allEntreprises: IEntreprise[];
   keyword: WritableSignal<string> = signal('');
   city: WritableSignal<string> = signal('');
+  entrepriseService = inject(EntrepriseService);
 
   cities: City[] = Object.keys(City).map((city) => city as City);
+
+  ngOnInit(): void {
+    this.entrepriseService
+      .getFakeEntrepriseData()
+      .subscribe((data: IEntreprise[]) => {
+        this.#allEntreprises = data;
+      });
+  }
 
   filteredEntreprises: Signal<IEntreprise[]> = computed(() =>
     this.#allEntreprises.filter(
@@ -49,133 +61,4 @@ export class HeroComponent {
           this.technology() == '')
     )
   );
-
-  getFakeEntrepriseData(): IEntreprise[] {
-    return [
-      {
-        name: 'FakeCorp',
-        city: City.Nouadhibou,
-        adresse: {
-          location: 'Your Company Address',
-          phoneNumber1: '+22223456789',
-          website: 'https://www.fakecorp.com',
-          socials: [
-            {
-              platform: SocialMediaPlatform.Linkedin,
-              value: 'https://www.linkedin.com/company/fakecorp',
-            },
-            {
-              platform: SocialMediaPlatform.Facebook,
-              value: 'https://www.facebook.com/fakecorp',
-            },
-            {
-              platform: SocialMediaPlatform.Instagram,
-              value: 'https://www.instagram.com/fakecorptheofficial',
-            },
-            {
-              platform: SocialMediaPlatform.WhatsApp,
-              value: 'https://chat.whatsapp.com/22223456789',
-            },
-            {
-              platform: SocialMediaPlatform.Youtube,
-              value: 'https://www.youtube.com/fakecorp',
-            },
-            {
-              platform: SocialMediaPlatform.Twitter,
-              value: 'https://twitter.com/FakeCorp',
-            },
-            {
-              platform: SocialMediaPlatform.Github,
-              value: 'https://github.com/FakeCorp',
-            },
-            {
-              platform: SocialMediaPlatform.Gitlab,
-              value: 'https://gitlab.com/fakecorp',
-            },
-            {
-              platform: SocialMediaPlatform.Telegram,
-              value: 'https://t.me/fakecorp',
-            },
-            {
-              platform: SocialMediaPlatform.Discord,
-              value: 'https://discord.gg/fakecorp',
-            },
-          ],
-        },
-        shortDescription: "FakeCorp : Connecter le monde, façonner l'avenir",
-        longDescription:
-          "A la pointe de l'innovation et d'excellence depuis 1975, Fake Corp. s'impose comme un acteur technologique majeur façonnant l'avenir. IA, logiciels, cloud computing et télécommunications : l'entreprise propose un panel de solutions de pointe pour accompagner les entreprises et les particuliers dans leur transition vers un avenir durable.",
-        sectors: ['Technology', 'AI', 'Software', 'Consulting'],
-        technologiesUsed: [
-          'Python',
-          'Angular',
-          'HTML',
-          'CSS',
-          'Tailwind',
-          'React',
-          'NextJS',
-          'JavaScript',
-          'AWS',
-        ],
-        logo: 'https://img.logoipsum.com/331.svg',
-      },
-      {
-        name: 'FakeComp',
-        city: City.Nouakchott,
-        adresse: {
-          location: 'Your Company Address',
-          phoneNumber1: '+22223456789',
-          website: 'https://www.fakecorp.com',
-          socials: [
-            {
-              platform: SocialMediaPlatform.Linkedin,
-              value: 'https://www.linkedin.com/company/fakecorp',
-            },
-            {
-              platform: SocialMediaPlatform.Facebook,
-              value: 'https://www.facebook.com/fakecorp',
-            },
-            {
-              platform: SocialMediaPlatform.Instagram,
-              value: 'https://www.instagram.com/fakecorptheofficial',
-            },
-            {
-              platform: SocialMediaPlatform.WhatsApp,
-              value: 'https://chat.whatsapp.com/22223456789',
-            },
-            {
-              platform: SocialMediaPlatform.Youtube,
-              value: 'https://www.youtube.com/fakecorp',
-            },
-            {
-              platform: SocialMediaPlatform.Twitter,
-              value: 'https://twitter.com/FakeCorp',
-            },
-            {
-              platform: SocialMediaPlatform.Github,
-              value: 'https://github.com/FakeCorp',
-            },
-            {
-              platform: SocialMediaPlatform.Gitlab,
-              value: 'https://gitlab.com/fakecorp',
-            },
-            {
-              platform: SocialMediaPlatform.Telegram,
-              value: 'https://t.me/fakecorp',
-            },
-            {
-              platform: SocialMediaPlatform.Discord,
-              value: 'https://discord.gg/fakecorp',
-            },
-          ],
-        },
-        shortDescription: "FakeComp : Connecter le monde, façonner l'avenir",
-        longDescription:
-          "A la pointe de l'innovation et d'excellence depuis 1975, Fake Corp. s'impose comme un acteur technologique majeur façonnant l'avenir. IA, logiciels, cloud computing et télécommunications : l'entreprise propose un panel de solutions de pointe pour accompagner les entreprises et les particuliers dans leur transition vers un avenir durable.",
-        sectors: ['Technology', 'AI', 'Software', 'Consulting'],
-        technologiesUsed: ['Azure', 'Flutter', 'HTML'],
-        logo: 'https://img.logoipsum.com/331.svg',
-      },
-    ];
-  }
 }
