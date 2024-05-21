4cf4a6a170e468da88e357e3d1c31d3734e6cc8f
Abdelhalim Jean <abdelhalimjean@gmail.com>
2024-05-21 16:48:07 +0000

    mention contributors in footer

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -7,20 +7,28 @@ import { bootstrapHeartFill } from '@ng-icons/bootstrap-icons';
   standalone: true,
   imports: [NgIcon],
   template: `
-    <footer class="sticky bottom-0 w-full py-4 text-center text-sm ">
-      <a
-        href="https://www.linkedin.com/in/abdelhalimjean/"
-        class="text-white/80 hover:text-white"
-      >
-        Made with
-        <ng-icon
-          name="bootstrapHeartFill"
-          class="text-byte-400 align-middle"
-        ></ng-icon>
-        by Abdelhalim Jean
-      </a>
+    <footer
+      class="sticky bottom-0 w-full py-4 text-center text-xs font-extralight text-white/60 hover:text-white/80"
+    >
+      Crée avec
+      <ng-icon
+        name="bootstrapHeartFill"
+        class="align-middle text-byte-400"
+      ></ng-icon>
+      par
+      <a href="https://www.linkedin.com/in/abdelhalimjean/">Abdelhalim Jean</a>
+      @if (totalContributors > 1) {
+        +
+        <a
+          href="https://github.com/abdelhalimjean/entreprises/graphs/contributors"
+        >
+          {{ totalContributors - 1 }} contributeurs
+        </a>
+      }
     </footer>
   `,
   providers: [provideIcons({ bootstrapHeartFill })],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  readonly totalContributors: number = 3;
+}
