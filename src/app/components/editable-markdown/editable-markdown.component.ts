9887cc9b964ee0586cedfc131248d4c4ce08e73a
Mbareck21 <missara.lamine@gmail.com>
2024-05-27 09:58:38 -0500

    initial commit

---
+++ src/app/components/editable-markdown/editable-markdown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editable-markdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './editable-markdown.component.html',
+  styleUrl: './editable-markdown.component.scss'
+})
+export class EditableMarkdownComponent {
+
+}
---
+++ src/app/components/markdown/markdown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './markdown.component.html',
+  styleUrl: './markdown.component.scss'
+})
+export class MarkdownComponent {
+
+}
