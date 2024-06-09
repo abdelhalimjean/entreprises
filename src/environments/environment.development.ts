518efcd40e877e0c93793361e7db905058a26f15
eljily <eljilysidibrahim@gmail.com>
2024-06-09 22:19:29 +0000

    removed process env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+  webhookUrl: 'webookh_url',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  webhookUrl: process.env['WEBHOOK_URL'] || '',
+  webhookUrl: 'webookh_url',
 };
